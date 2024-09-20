16e23a0b6e9f751577e11904364925378b83f0cb
Connor Sheehan <cosheehan@mozilla.com>
2024-09-20 19:55:59 -0400

    debugging

--- tests/test_style.py
+++ tests/test_style.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import os
 import subprocess
 import sys
 from pathlib import Path
@@ -18,7 +19,23 @@ PY_FILES = sorted(
 
 
 def test_black():
+    print(f"pytest {sys.executable=}")
+    print(f"pytest {os.getenv('PYTHONPATH')=}")
+
     print(subprocess.check_call([sys.executable, "-m", "pip", "freeze"]))
+
+    subprocess.check_call([sys.executable, "-c", "import click"])
+
+    try:
+        import click  # noqa: F401
+
+        print("click imported without error")
+    except ImportError as e:
+        print(f"error importing click: {str(e)}")
+
+    subprocess.check_call([sys.executable, "-m", "black", "--version"])
+    subprocess.check_call(["black", "--version"])
+    subprocess.check_call([find_script_path("black"), "--version"])
     subprocess.check_call([sys.executable, "-m", "black", "--check"] + PY_FILES)
 
 
